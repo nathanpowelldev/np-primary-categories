5a66cc10d99c1d74937c656cf7db527a4883a834
Nathan Powell <nathan@brightfiregroup.com>
2019-04-09 11:49:30 -0400

    initialize git

---
+++ primary-categories.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name:	NP Primary Categories
+Plugin URI:		https://github.com/nathanpowelldev/np-primary-categories
+Description:	Adds ability to assign a primary category to posts.
+Version:		1.0
+Author:			Nathan Powell
+Author URI:		https://github.com/nathanpowelldev
+License: 		GPL2
+
+NP Primary Categories is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+NP Primary Categories is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with NP Primary Categories. If not, see http://www.gnu.org/licenses/gpl.html.
+*/
